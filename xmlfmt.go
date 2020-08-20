403af8ba6689f93a9dfd6606ccc782f5d4edc4e8
defektive <defektive@users.noreply.github.com>
2020-08-20 10:37:11 -0600

    close enough

--- xmlfmt.go
+++ xmlfmt.go
@@ -13,12 +13,13 @@ import (
 
 var (
 	reg = regexp.MustCompile(`<([/!]?)([^>]+?)(/?)>`)
-	// NL is the newline string used in XML output, define for DOS-convenient.
-	NL = "\r\n"
+	// NL is the newline string used in XML output
+	NL = "\n"
 )
 
 // FormatXML will (purly) reformat the XML string in a readable way, without any rewriting/altering the structure
 func FormatXML(xmls, prefix, indent string) string {
+	// replace all whitespace between tags
 	src := regexp.MustCompile(`(?s)>\s+<`).ReplaceAllString(xmls, "><")
 
 	rf := replaceTag(prefix, indent)
@@ -29,31 +30,50 @@ func FormatXML(xmls, prefix, indent string) string {
 // and deal with comments as well
 func replaceTag(prefix, indent string) func(string) string {
 	indentLevel := 0
+	justOpened := false
 	return func(m string) string {
 		// head elem
 		if strings.HasPrefix(m, "<?xml") {
+			justOpened = false
 			return prefix + strings.Repeat(indent, indentLevel) + m + NL
 		}
+
 		// empty elem
 		if strings.HasSuffix(m, "/>") {
-			return prefix + strings.Repeat(indent, indentLevel) + m + NL
+			if justOpened {
+				justOpened = false
+				return NL + prefix + strings.Repeat(indent, indentLevel) + m + NL
+			} else {
+				return prefix + strings.Repeat(indent, indentLevel) + m + NL
+			}
 		}
 		// comment elem
 		if strings.HasPrefix(m, "<!") {
-			return NL + prefix + strings.Repeat(indent, indentLevel) + m 
+			justOpened = false
+			return NL + prefix + strings.Repeat(indent, indentLevel) + m
 		}
 		// end elem
 		if strings.HasPrefix(m, "</") {
-			
 			defer func() {
-				indentLevel--
+				justOpened = false
 			}()
-			return m + NL
+			indentLevel--
+			if justOpened {
+				return m + NL
+			}
+			return prefix + strings.Repeat(indent, indentLevel) + m + NL
 		}
-// 		defer func() {
-			indentLevel++
-// 		}()
 
-		return NL + prefix + strings.Repeat(indent, indentLevel) + m
+		defer func() {
+			indentLevel++
+		}()
+
+		if justOpened {
+			// indentLevel++
+			return NL + prefix + strings.Repeat(indent, indentLevel) + m
+		}
+
+		justOpened = true
+		return prefix + strings.Repeat(indent, indentLevel) + m
 	}
 }
